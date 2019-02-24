e353d8bd04869fd8ef925c2a11860dd1746bb497
ICEPOR\asharma2 <asharma2@ivycomptech.com>
2019-02-24 22:18:13 +0530

    first commit

---
+++ BlazorWithFirestore.Shared/Models/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Google.Cloud.Firestore;
+
+namespace BlazorWithFirestore.Shared.Models
+{
+    [FirestoreData]
+    public class Employee
+    {
+        public string EmployeeId { get; set; }
+        public DateTime date { get; set; }
+        [FirestoreProperty]
+        public string EmployeeName { get; set; }
+        [FirestoreProperty]
+        public string CityName { get; set; }
+        [FirestoreProperty]
+        public string Designation { get; set; }
+        [FirestoreProperty]
+        public string Gender { get; set; }
+    }
+}
